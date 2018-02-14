9081dd9f8167f764ad614c04d9abf67e195acf54
Filipe Farinha <filipe@ktorn.com>
2018-02-14 16:11:58 +0800

    Revised doc and added it to README.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # reverse-ssh-tunnel
-Bash script to establish an automatic and permanent SSH tunnel with reversed port forwarding connection to a remote host.
+
+Requires: `autossh`
+
+Bash script to establish an automatic and permanent SSH tunnel with reversed port forwarding.
+
+This allows connections to a remote (public) host to be forwarded to a host behind a NAT in the local network.  
+
+Disconnections and re-connections will be gracefully handled by autossh. For this to work you should setup authentication using SSH keys.
+
+It does the following:
+
+1. Establishes an SSH connection to `REMOTE_HOST`;
+2. binds `REMOTE_HOST_TUNNEL_PORT` in `REMOTE_HOST`;
+3. forwards incoming connections from  
+   `REMOTE_HOST`:`REMOTE_HOST_TUNNEL_PORT`  
+   to  
+   `LOCAL_HOST`:`LOCAL_HOST_TUNNEL_PORT`.
+
+**Note:** In order to bind `REMOTE_HOST_TUNNEL_PORT` to all interfaces in `REMOTE_HOST`, you may need to add `'GatewayPorts yes'` to the sshd configuration of `REMOTE_HOST`. Remember to restart the sshd daemon after making changes to its configuration.  
+Failing to do this may result in the tunnel refusing external connections.  
+
+Also, `LOCAL_HOST` is relative to the host where you run this script from, and it can either be `localhost` itself, or any other host accessible by it.
+
+Default configuration forwards `REMOTE_HOST`:`8888` to `LOCAL_HOST`:`80`.
+
+Copyright (C) 2015-2018 Filipe Farinha - All Rights Reserved
+Permission to copy and modify is granted under the GPLv3 license
--- reverse-tunnel.sh
+++ reverse-tunnel.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
-# Establishes a persistent outgoing SSH connection with reverse port forwarding.
-# This allows remote connections to a host behind local network's NAT.
-# Disconnections will be gracefully handled by autossh, reconnecting as required.
+#
 # Requires: autossh
 #
-# It establishes an SSH connection to REMOTE_HOST, binding REMOTE_HOST_TUNNEL_PORT to
-# LOCAL_HOST's LOCAL_HOST_TUNNEL_PORT.
+# Establishes a persistent outgoing SSH connection with reverse port forwarding.
+# This allows connections to a remote (public) host to be forwarded to a host
+# behind a NAT in the local network.
+# Disconnections and re-connections will be gracefully handled by autossh. For this
+# to work you should setup automatic authentication using SSH keys.
 #
-# Note: In order to bind the reverse port to all interfaces in REMOTE_HOST,
+# It does the following:
+# 1) Establishes an SSH connection to REMOTE_HOST;
+# 2) binds REMOTE_HOST_TUNNEL_PORT in REMOTE_HOST;
+# 3) forwards incoming connections to REMOTE_HOST_TUNNEL_PORT to
+#    LOCAL_HOST_TUNNEL_PORT in LOCAL_HOST.
+#
+# Note: In order to bind REMOTE_HOST_TUNNEL_PORT to all interfaces in REMOTE_HOST,
 #       you may need to add 'GatewayPorts yes' to the sshd configuration of REMOTE_HOST.
-#       Also, LOCAL_HOST is the target host as seen by the host you run this script from,
-#       and can either be localhost itself, or any other host accessible by it.
-#
-# Default configuration forwards REMOTE_HOST port 2222 to LOCAL_HOST port 22.
+#       Failing to do this may result in the tunnel refusing external connections.
+#       Also, LOCAL_HOST is relative to the host where you run this script from,
+#       and can either be `localhost` itself, or any other host accessible by it.
 #
 # Copyright (C) 2015-2016 Filipe Farinha - All Rights Reserved
 # Permission to copy and modify is granted under the GPLv3 license
 # Last revised 14/02/2018
 
-LOCAL_HOST=localhost
-LOCAL_HOST_TUNNEL_PORT=22
+# Default configuration forwards REMOTE_HOST port 8888 to LOCAL_HOST port 80.
 
+# The remote host that we connect to via SSH, and establish the listening remote port
 REMOTE_HOST=remotehost.example.net
 REMOTE_HOST_SSH_PORT=22
 REMOTE_HOST_SSH_USER=myusername
+REMOTE_HOST_TUNNEL_PORT=8888
 
-REMOTE_HOST_TUNNEL_PORT=2222
+# The local host is the destination of traffic received by the remote port
+LOCAL_HOST=localhost
+LOCAL_HOST_TUNNEL_PORT=80
 
 # Ignore early failed connections at boot
 export AUTOSSH_GATETIME=0
